a2f57dc433544c70b7cb7f9bfd9f012303660459
Tanya <6ateni@gmail.com>
2024-02-13 12:41:42 +0300

    add homework

---
+++ задача 2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+int FindAckerman(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return FindAckerman(n - 1, 1);
+    }
+    else
+    {
+        return FindAckerman(n - 1, FindAckerman(n, m - 1));
+    }
+}
+Console.Write("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+FindAckerman(M, N);
+Console.Write($"Функция Аккермана = {FindAckerman(M, N)}");
---
+++ задача 3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 3: Задайте произвольный массив.
+// Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+int[] FillArray(int size, int minRange, int maxRange)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minRange, maxRange + 1);
+    }
+    return arr;
+}
+Console.Write("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(N, 0, 100);
+// Размер массива: N, элементы: [0,100]
+Console.WriteLine($"{string.Join(" ", array)}");
+
+int size = array.Length;
+
+void ShowArrayReverse(int[] array, int size)
+{
+    int j = size - 1;
+    if (j == 0)
+    {
+        Console.WriteLine(array[j]);
+        return;
+    }
+
+    Console.Write(array[j] + " ");
+    ShowArrayReverse(array, size - 1);
+}
+ShowArrayReverse(array, size);
